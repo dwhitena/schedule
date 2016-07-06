ed49c6106b93825cdf0147815954800a085761ad
Daniel Whitenack <whitenack.daniel@gmail.com>
2016-07-06 14:56:17 -0500

    naming

--- gophercon2016/pushtopach.go
+++ gophercon2016/pushtopach.go
@@ -32,24 +32,24 @@ func pushToPach(filename string) error {
 	}
 
 	// Create a repo called "filer"
-	if err := c.CreateRepo("filter"); err != nil {
+	if err := c.CreateRepo("godata"); err != nil {
 		errors.Wrap(err, "Could not create pachyderm repo")
 	}
 
 	// Start a commit in our new repo on the "master" branch
-	_, err = c.StartCommit("filter", "", "master")
+	_, err = c.StartCommit("godata", "", "master")
 	if err != nil {
 		errors.Wrap(err, "Could not start pachyderm repo commit")
 	}
 
 	r := bytes.NewReader(csvfile)
 	// Put a file in the newly created commit.
-	if _, err := c.PutFile("filter", "master", filename, r); err != nil {
+	if _, err := c.PutFile("godata", "master", filename, r); err != nil {
 		errors.Wrap(err, "Could not put file into pachyderm repo")
 	}
 
 	// Finish the commit.
-	if err := c.FinishCommit("filter", "master"); err != nil {
+	if err := c.FinishCommit("godata", "master"); err != nil {
 		errors.Wrap(err, "Could not finish Pachyderm commit")
 	}
 
