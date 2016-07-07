365b3569927c395c323353881dc12c40e0b29713
William Kennedy <bill@ardanlabs.com>
2016-07-07 14:53:04 -0400

    Minor changes to the code examples.

--- gophercon2016/code/getrepos/getrepos.go
+++ gophercon2016/code/getrepos/getrepos.go
@@ -1,9 +1,8 @@
 // All material is licensed under the Apache License Version 2.0, January 2004
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+
 // This example demonstrates how to scrape data about Go github repositories
 // using the github.com/google/go-github/github package.
-//
 package main
 
 import (
@@ -18,62 +17,58 @@ import (
 
 func main() {
 
-	// t1 is the date from which we will start scraping github data.
+	// Capture the date from which we will start scraping github data.
 	t1 := time.Date(2016, time.July, 1, 0, 0, 0, 0, time.UTC)
 
-	// queryFromStartTime is evaluated to pull the github data
-	// from t1 to now.
+	// Pull the github data from t1 to now.
 	if err := queryFromStartTime(t1); err != nil {
 		log.Fatal(err)
 	}
-
 }
 
-// queryFromStartTime queries github for all 2 day time ranges of repo create dates
-// from a start time until now
+// queryFromStartTime queries github for all 2 day time ranges of repo create
+// dates from a start time until now.
 func queryFromStartTime(t1 time.Time) error {
 
-	// client is the github client.
+	// Create a new github client.
 	client := github.NewClient(nil)
 
-	// We loop over 2 days periods from the start time until now
-	// such that the number of github repos we return is less than the
-	// 10 pages X 100 results limit of the github API.
+	// Loop over 2 days periods from the start time until now such that
+	// the number of github repos we return is less than the 10 pages X
+	// 100 results limit of the github API.
 	for t1.Unix() < time.Now().Unix() {
 
-		// The github query string is generated using fmt.
+		// Create the github query string.
 		t2 := t1.Add(time.Hour * 24 * 2)
 		tString := fmt.Sprintf("\"%d-%02d-%02d .. %d-%02d-%02d\"",
 			t1.Year(), t1.Month(), t1.Day(),
 			t2.Year(), t2.Month(), t2.Day())
 		query := fmt.Sprintf("language:Go created:" + tString)
 
-		// clientQuery then uses the generated query string to query github
+		// Query github with our new query.
 		err := clientQuery(client, query)
 		if err != nil {
 			errors.Wrap(err, "Could not search Github repos")
 		}
 
-		// At the end of each loop, the start time of the 2 day chunk is
-		// incremented.
+		// Increment the start time of the 2 day chunk.
 		t1 = t1.Add(time.Hour * 24 * 2)
-
 	}
 
 	return nil
-
 }
 
-// clientQuery executes github queries and searches over all pages of a result set
-// parsing results
+// clientQuery executes github queries and searches over all pages of a result
+// set parsing results.
 func clientQuery(gh *github.Client, query string) error {
 
-	// page will keep track of the github search page we are currently searching,
-	// and maxpage will track the maximum page of github results for a given query.
+	// Set the github search page we are currently searching.
 	page := 1
+
+	// Set the maximum page of github results for a given query.
 	maxPage := math.MaxInt32
 
-	// opts contains the github search options for the query.
+	// Set the github search options for the query.
 	opts := github.SearchOptions{
 		Sort:  "stars",
 		Order: "desc",
@@ -82,8 +77,8 @@ func clientQuery(gh *github.Client, query string) error {
 		},
 	}
 
-	// This loop loops over pages in the github results so we can gather all of the
-	// repositories in the search results.
+	// Loop over pages in the github results so we can gather all
+	// of the repositories in the search results.
 	for page <= maxPage {
 
 		// We can utilized the Search.Repositories method to Query github
@@ -93,13 +88,15 @@ func clientQuery(gh *github.Client, query string) error {
 		if err != nil {
 			return errors.Wrap(err, "Could not search Github result pages")
 		}
+
+		// Wait for the results.
 		Wait(response)
 		maxPage = response.LastPage
 
 		// Then we loop over the repositories in the search results.
 		for _, repo := range result.Repositories {
 
-			// Data of interested in extracted.
+			// Extract the data of interest.
 			name := *repo.FullName
 			updatedAt := repo.UpdatedAt.String()
 			createdAt := repo.CreatedAt.String()
@@ -108,25 +105,22 @@ func clientQuery(gh *github.Client, query string) error {
 			stars := *repo.StargazersCount
 			size := *repo.Size
 
-			// Here we print out the results for now.  However, this can be
+			// Print out the results for now.  However, this can be
 			// redirected to a CSV file if desired.
 			fmt.Printf("%s,%s,%s,%d,%d,%d,%d\n",
 				name, updatedAt, createdAt, forks, issues, stars, size)
-
 		}
 
-		// The program sleeps for 10 seconds to stay within Github's rate
-		// limiting constraints, then it increments the page and query again.
+		// Sleep for 10 seconds to stay within Github's rate limiting
+		// constraints, then it increments the page and query again.
 		time.Sleep(time.Second * 10)
 		page++
-
 	}
 
 	return nil
-
 }
 
-// Wait waits to make sure we return the full github response
+// Wait waits to make sure we return the full github response.
 func Wait(response *github.Response) {
 	if response != nil && response.Remaining <= 1 {
 		gap := time.Duration(response.Reset.Local().Unix() - time.Now().Unix())
--- gophercon2016/code/pushtopach/pushtopach.go
+++ gophercon2016/code/pushtopach/pushtopach.go
@@ -1,12 +1,11 @@
 // All material is licensed under the Apache License Version 2.0, January 2004
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+
 // This example demonstrates how to version a file in pachyderm's PFS file system.
 // Note, the below logic assumes pachyderm is running on localhost.  However,
 // localhost:30650 could be change to any <host>:30650 on which pachyderm is
 // running.  See http://pachyderm.io/ for more details on getting started with
 // pachyderm.
-//
 package main
 
 import (
@@ -20,52 +19,48 @@ import (
 
 func main() {
 
-	// Here we are pushing the file "repodata.csv" into pachyderm's PFS file system.
+	// Push the file "repodata.csv" into pachyderm's PFS file system.
 	if err := pushToPach("repodata.csv"); err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 // pushToPach reads in the given file, create a repo in PFS for the file, opens
 // a commit, pushes the file in the commit, and finishes the commit.
 func pushToPach(filename string) error {
 
-	// First we read the contents of the given file.
+	// Read the contents of the given file.
 	csvfile, err := ioutil.ReadFile(filename)
 	if err != nil {
 		errors.Wrap(err, "Could not read input file")
 	}
 
-	// Then we open a connection to pachyderm running on localhost.
+	// Open a connection to pachyderm running on localhost.
 	c, err := client.NewFromAddress("localhost:30650")
 	if err != nil {
 		errors.Wrap(err, "Could not connect to Pachyderm")
 	}
 
-	// A repo called "godata" is created.
+	// Create a repo called "godata".
 	if err := c.CreateRepo("godata"); err != nil {
 		errors.Wrap(err, "Could not create pachyderm repo")
 	}
 
-	// We start a commit on the "master" branch of the godata repo.
-	_, err = c.StartCommit("godata", "", "master")
-	if err != nil {
+	// Start a commit on the "master" branch of the godata repo.
+	if _, err = c.StartCommit("godata", "", "master"); err != nil {
 		errors.Wrap(err, "Could not start pachyderm repo commit")
 	}
 
-	// Then after the commit is started, we can put the given file into
-	// the godata repo.
+	// Put the given file into the godata repo after the commit is started.
 	r := bytes.NewReader(csvfile)
 	if _, err := c.PutFile("godata", "master", filename, r); err != nil {
 		errors.Wrap(err, "Could not put file into pachyderm repo")
 	}
 
-	// Lastly, we "finish" the commit.
+	// Finish the commit.
 	if err := c.FinishCommit("godata", "master"); err != nil {
 		errors.Wrap(err, "Could not finish Pachyderm commit")
 	}
 
 	return nil
-
 }
